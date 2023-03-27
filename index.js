0cd25fcc6123ce767915fa8e7fb370fa0c4087c3
Henrike Stahlhut <henrike.st@icloud.com>
2023-03-27 20:59:44 +0200

    run test

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function toggleButton() {
-  const cards = document.querySelectorAll("[data-js=card]");
-  const buttons = card.querySelectorAll("[data-js=buttons]");
-  cards.forEach(card => {
-        const button = card.querySelector('[data-js="button"]')
-        const link =  cards.querySelector('[data-js="link"]')
-        const arrow = card.querySelector('[data-js="arrow"]')
-          button.addEventListener('click',() => {
-          link.classList.toggle("hide")
-          arrow.classList.toggle("rotate")
-          
-    })
-})
-}
+const cards = document.querySelectorAll('[data-js=card]');
+// const buttons = document.querySelectorAll('[data-js=buttons]');
+
+cards.forEach(() => {
+  const button = document.querySelector('[data-js="button"]');
+  const link = document.querySelector('[data-js="link"]');
+  const arrow = document.querySelector('[data-js="arrow"]');
+
+  button.addEventListener('click', () => {
+    link.classList.toggle('hide');
+    arrow.classList.toggle('rotate');
+  });
+});
